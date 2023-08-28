55ce094eeac33a4169b93706a1c6c040a57134a3
Viru <brandon.jvc@gmail.com>
2023-08-27 22:27:29 -0500

    Finalizacion de la rama master

---
+++ Models/prueba.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practica1_viru.Models
+{
+    public class prueba
+    {
+
+
+        public string prueba1 { get; set; }
+    }
+}
